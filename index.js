f65f9b44e3aaa30c7a02e8441704c074a4ce81b9
Theodor Vararu <theo.vararu@gamesys.co.uk>
2018-06-11 10:21:52 +0100

    R e i m p l e m e n t  l o g i c  t o  a l t s p a c e s

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
 #!/usr/bin/env node
-module.exports = altcaps
-function altcaps(words) {
-  const letters = words.toString('utf8')
-  const methods = ['Upper', 'Lower']
-  var idx = 0
-  var midx = (Math.random() * 100)|0
-  var newstring = ''
-  var letter
-  for (; idx < letters.length; idx++) {
-    letter = letters[idx]
-    newstring += (letter.match(/\s/))
-      ? letter
-      : letter['to' + methods[midx++ % 2] + 'Case']()
-  }
-  return newstring
+module.exports = altspaces;
+function altspaces(words) {
+  return words.split("").join(" ");
 }
 
 if (!module.parent) {
-  const concat = require('concat-stream')
-  process.stdin.resume()
-  process.stdin.pipe(concat(function (data) {
-    process.stdout.write(altcaps(data.toString()))
-  }))
+  const concat = require("concat-stream");
+  process.stdin.resume();
+  process.stdin.pipe(
+    concat(function(data) {
+      process.stdout.write(altspaces(data.toString()));
+    })
+  );
 }
